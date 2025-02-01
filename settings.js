bb7f38047e50c696c608d98bcb21bfb1703e66f6
samanediri <Samanedirimuni1515@gmail.com>
2025-02-01 11:29:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 //👉 Put Your Session Id 🐣 👈කියන එක කපලා අයින් කරලා ඔයාගේ Session id එක දාන්න.
 
-SESSION_ID: process.env.SESSION_ID || "Put Your Session Id 🐣",
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "𝐘𝐨𝐮𝐫 𝐒𝐭𝐚𝐭𝐮𝐬 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐜𝐥𝐲 𝐒𝐞𝐞𝐧𝐬 𝐁𝐲 𝐋𝐚𝐤𝐚-𝐌𝐝 🧚‍♀️🐣💗",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=cgIRXJyJ#Y-P4zoinwOsoXTcqGvg9whZfcmMjVZ63oV4nZX4C2w8",
+AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "𝐘𝐨𝐮𝐫 𝐒𝐭𝐚𝐭𝐮𝐬 𝐀𝐮𝐭𝐨𝐦𝐚𝐭𝐢𝐜𝐥𝐲 𝐒𝐞𝐞𝐧𝐬 𝐁𝐲 ShÆňüwæ 🧚‍♀️🐣💗",
 
 
 //=================================================
